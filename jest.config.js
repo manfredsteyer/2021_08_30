c5831f906179d415f89248c5d6acb57ffe9a7fa0
Manfred Steyer <manfred.steyer@gmx.net>
2021-08-31 09:36:02 +0200

    day 1

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  projects: [
+    '<rootDir>/apps/flight-app',
+    '<rootDir>/libs/flight-lib',
+    '<rootDir>/libs/logger-lib',
+    '<rootDir>/libs/dashboard-lib',
+    '<rootDir>/apps/flight-admin',
+    '<rootDir>/apps/dashboard',
+    '<rootDir>/apps/external',
+    '<rootDir>/libs/passenger/domain',
+    '<rootDir>/apps/passenger',
+    '<rootDir>/libs/passenger/feature-search',
+    '<rootDir>/libs/passenger/feature-edit',
+    '<rootDir>/libs/styling',
+    '<rootDir>/libs/luggage/domain',
+    '<rootDir>/apps/luggage',
+    '<rootDir>/libs/luggage/feature-checkin',
+    '<rootDir>/libs/luggage/feature-report-loss',
+    '<rootDir>/libs/shared/util-auth',
+  ],
+};
---
+++ libs/logger-lib/src/index.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of logger-lib
+ * Public API // Facade
+ */
+export * from './lib/logger.config';
+export * from './lib/logger.service';
+export * from './lib/log-monitor.component';
+export * from './lib/logger.module';
